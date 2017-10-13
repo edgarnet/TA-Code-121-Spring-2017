b3debb1cc052ddeef2dc5924bf0a0c8dce993ff2
Edgar Perez <ultra@localhost.localdomain>
2017-10-13 02:48:27 -0700

    Preprocessor Update

--- ANSI.c
+++ ANSI.c
@@ -1,26 +1,50 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ANSI.c
+ *
+ *    Description:  Allows for cross-platform implementation of ANSI escape sequences.
+ *
+ *        Version:  1.0
+ *        Created:  10/12/2017 08:18:17 PM
+ *       Revision:  Many
+ *       Compiler:  Visual C, GCC
+ *
+ *         Author:  Edgar Perez (eap), edgar.a.perez@wsu.edu
+ *   Organization:  Washington State University
+ *
+ * =====================================================================================
+ */
+
+
 // This goes in your header file.
 #include	<stdio.h>
 #define		CLS		"\033[2J\033[1;1H"
 
 #ifdef _WIN32
+
 #include	<windows.h>
 #ifndef ENABLE_VIRTUAL_TERMINAL_PROCESSING
 #define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
-#endif
+
+	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	DWORD dwMode = 0;
+	GetConsoleMode(hOut, &dwMode);
+	dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+	SetConsoleMode(hOut, dwMode);
+
 #endif
 
-// Lines 14-20 go in your main function.
+#endif
+
 int main(void) {
-	if (_WIN32) {
-		HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-		DWORD dwMode = 0;
-		GetConsoleMode(hOut, &dwMode);
-		dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
-		SetConsoleMode(hOut, dwMode);
-	}
+
 	printf("Enter input: \n");
+	// This is a good substitute for 'system("pause")'.
 	getchar();
+	// This is where you would print the 'clear screen' escape sequence.
 	printf("%s", CLS);
-	printf("Success!");
+	printf("Is it clear?");
 	return 0;
+
 }
